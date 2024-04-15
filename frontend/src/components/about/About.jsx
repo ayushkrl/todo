2350d30f2b6f2e0c9865c8645c769a91dbad766b
ayushkrl <111305534+ayushkrl@users.noreply.github.com>
2024-04-15 16:20:05 +0530

    Add files via upload

---
+++ frontend/src/components/about/About.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./about.css";
+const About = () => {
+  return (
+    <div className="about d-flex justify-content-center align-items-center">
+      <div className="container">
+        <div className="d-flex">
+          <h1>About Us</h1>
+        </div>
+
+        <p>
+          This website is developed by Ayush Kumar Lal,
+          National Institute of Technology, Jamshedpur, Bachelor of Technology in
+          Electronics and Communication Engineering specialization.
+          <br/><br/><br/><br/><br/>
+          ALL RIGHTS RESERVED
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
